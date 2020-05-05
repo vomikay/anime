90f50bfc32c9f20905333b8d034bedae8376a179
Yakimov Alexander <yakimov.alex2354@gmail.com>
2020-05-05 07:31:30 +0300

    set mui palette

--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,24 @@
 import { createMuiTheme } from "@material-ui/core/styles";
 
-const theme = createMuiTheme();
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      dark: "#f57c00",
+      main: "#ff9800",
+      light: "#ffe0b2",
+    },
+    secondary: {
+      main: "#03a9f4",
+    },
+    background: {
+      default: "#fff",
+    },
+    text: {
+      primary: "#212121",
+      secondary: "#757575",
+    },
+    divider: "#bdbdbd",
+  },
+});
 
 export default theme;
